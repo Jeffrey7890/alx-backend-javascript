2e4412218115e357b47faef7805ca18d538b9486
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-09-30 22:36:58 +0000

    new arrow

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,8 @@
 export default function getNeighborhoodsList() {
 	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-	const self = this;
-	const add = (newNeighborhood) => {
-		self.sanFranciscoNeighborhoods.push(newNeighborhood);
-		return self.sanFranciscoNeighborhoods;
+	this.addNeighborhood = (newNeighborhood) => {
+		this.sanFranciscoNeighborhoods.push(newNeighborhood);
+		return this.sanFranciscoNeighborhoods;
 	};
-	self.addNeighborhood = add;
 }
