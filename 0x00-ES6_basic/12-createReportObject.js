0fc792bc586ac8f6df35cc01d9dc72d7d0c6482c
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-10-01 01:12:56 +0000

    numbers 11, 12 and package.json config

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList }, // Spread the employeesList into allEmployees
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length; // Return the number of departments
+    }
+  };
+}
+
