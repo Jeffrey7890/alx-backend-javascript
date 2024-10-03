01359424e8ba3a84b17f174e637269f7f902a915
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-10-03 01:25:51 +0000

    promises.all2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,11 @@ import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   const promises = [uploadPhoto(), createUser()];
+  let ans = '';
   Promise.all(promises)
     .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
+      ans = `${results[0].body} ${results[1].firstName} ${results[1].lastName}`;
+      console.log(ans);
     })
     .catch(() => console.log('Signup system offline'));
 }
