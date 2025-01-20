1bd38fe23425e6840c184c797916ca9e62527de1
jeffrey7890 <d3v3loperpy@gmail.com>
2025-01-20 23:05:23 +0000

    fixed error

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello ALX!');
+  if (req.url === '/' || req.url.startsWith('/')) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello ALX!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
 
 app.listen(1245, () => {
